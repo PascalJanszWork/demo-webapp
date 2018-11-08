9dcea1dbe3c5763bab915dcc96c7d302a7800d8c
Pascal Jansz <pascal.jansz@asml.com>
2018-11-08 07:46:36 +0100

    Added dev Env

--- Startup.cs
+++ Startup.cs
@@ -27,7 +27,7 @@ namespace demo_webapp
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello Development!");
             });
         }
     }
